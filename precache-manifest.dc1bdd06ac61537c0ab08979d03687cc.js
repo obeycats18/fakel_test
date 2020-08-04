fa4fcd278ab3091cd59b65f89d5c1ed3bd96782e
obeycats18@gmail.com <hA9FrW*R3^Y2>
2020-08-04 22:18:26 +0300

    Updates

---
+++ precache-manifest.dc1bdd06ac61537c0ab08979d03687cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6274cc9d70d3cdbf80938fc2c3e32b2",
+    "url": "/fakel_test/index.html"
+  },
+  {
+    "revision": "bad867a13401a2f4ba2a",
+    "url": "/fakel_test/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "e211eb1784b1e5e1b7a3",
+    "url": "/fakel_test/static/js/2.b3d089d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fakel_test/static/js/2.b3d089d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad867a13401a2f4ba2a",
+    "url": "/fakel_test/static/js/main.65a4fb99.chunk.js"
+  },
+  {
+    "revision": "98acbae1ed2f3c6a1599",
+    "url": "/fakel_test/static/js/runtime-main.1109dbe9.js"
+  }
+]);
